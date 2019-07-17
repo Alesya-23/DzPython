e12700a01f78d5c67fd24c2cc98832a1dfaa62a8
Alesya-23 <52533375+Alesya-23@users.noreply.github.com>
2019-07-17 15:50:51 +0400

    Add files via upload

---
+++ # 1 часть ДЗ.py
@@ -0,0 +1,30 @@
+print("1 часть ДЗ")
+x=0
+while x < 100:
+    x+=1
+    if x%3==0 :
+        print('Fizz')
+    elif x%5==0:
+        print('Buzz')
+    elif x%3==0 & x%5==0:
+        print('FizzBuzz')
+    else:
+        print(x)
+print("2часть ДЗ")
+dict={b:b**3 for b in range(50)}
+print("Cловарь старый вариант\n", dict, "\n")
+dict2={}
+for i in dict.keys():
+	o={dict[i]:i}
+	dict2.update(o)
+dict=dict2
+print("Новый словарь", dict2)
+
+print("3 часть ДЗ")
+m=[1,2,1,2,3,4,5]
+b=[]
+for i in m:
+    if i not in b:
+        b.append(i)
+print(b)
+
